b2960eac91c4b352deb9878d22b7ebf1e9da8bcf
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-04-23 12:50:27 +0530

    Update backups.py

--- MashaRoBot/modules/backups.py
+++ MashaRoBot/modules/backups.py
@@ -325,7 +325,7 @@ def export_data(update, context):
         },
     }
     baccinfo = json.dumps(backup, indent=4)
-    with open("SenkuRobot{}.backup".format(chat_id), "w") as f:
+    with open("missmiakhalifabot{}.backup".format(chat_id), "w") as f:
         f.write(str(baccinfo))
     context.bot.sendChatAction(current_chat_id, "upload_document")
     tgl = time.strftime("%H:%M:%S - %d/%m/%Y", time.localtime(time.time()))
@@ -341,15 +341,15 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("SenkuRobot{}.backup".format(chat_id), "rb"),
-        caption="💾*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `SenkuRobot-Backup` was specially made for notes 📚.".format(
+        document=open("missmiakhalifabot{}.backup".format(chat_id), "rb"),
+        caption="💾*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `missmiakhalifabot-Backup` was specially made for notes 📚.".format(
             chat.title, chat_id, tgl
         ),
         timeout=360,
         reply_to_message_id=msg.message_id,
         parse_mode=ParseMode.MARKDOWN,
     )
-    os.remove("SenkuRobot{}.backup".format(chat_id))  # Cleaning file
+    os.remove("missmiakhalifabot{}.backup".format(chat_id))  # Cleaning file
 
 
 # Temporary data
